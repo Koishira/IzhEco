7a26b7d667f7371654940a573fa976bf1e2c5d84
Koishira <lds.udm7723@gmail.com>
2023-04-26 23:03:29 +0400

    Delete button

--- app/src/main/java/com/example/izheco/MainActivity.java
+++ app/src/main/java/com/example/izheco/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.searchbutton);
     }
 }
